106ca53b3d5fc6a408428fbb0d2d70f44d3af0a9
Morten Egan <morten@egan.dk>
2013-07-20 23:04:21 +0200

    Added Git/Github init, subscribe support

--- QuickStarts/crp.sh
+++ QuickStarts/crp.sh
@@ -17,20 +17,30 @@ else
 	echo "Project $PROJECTNAME readme file" >> $PROJECTHOME/README.md
 	# Create GIT repository for project
 	cd $PROJECTHOME
-	git init
-	git add README.md
-	git commit -m "$PROJECTNAME first commit"
+	git init >> $HOME/create_project_$PROJECTNAME.log
+	git add README.md >> $HOME/create_project_$PROJECTNAME.log
+	git commit -m "$PROJECTNAME first commit" >> $HOME/create_project_$PROJECTNAME.log
 	# Create repository on GitHub
-	# Expect GIT OAuth token to be located in $HOME/Access/.git_oauth_token
+	# Expect Github OAuth token to be located in $HOME/Access/.git_oauth_token
+	# Expect Github username to be located in $HOME/Access/.git_username
+	# Expect Github password to be located in $HOME/Access/.git_password
 	if [ -f $HOME/Access/.git_oauth_token ]; then
 		git_token=$(<$HOME/Access/.git_oauth_token)
-		echo $git_token
+		git_username=$(<$HOME/Access/.git_username)
+		git_password=$(<$HOME/Access/.git_password)
+		# Create new repository
 		curl -i -H "Authorization: token $git_token" \
-     	-d '{ "name": "$PROJECTNAME" }' \
-      	https://api.github.com/user/repos
+     	-d "{ \"name\": \"$PROJECTNAME\" }" \
+      	https://api.github.com/user/repos >> $HOME/create_project_$PROJECTNAME.log
+      	# Add remote repository
+      	git remote add origin https://github.com/$git_username/$PROJECTNAME.git >> $HOME/create_project_$PROJECTNAME.log
+      	# Set remote URL to include password so remote push always is automatic.
+      	git remote set-url origin https://$git_username:$git_password@github.com/$git_username/$PROJECTNAME.git >> $HOME/create_project_$PROJECTNAME.log
+      	git push -u origin master >> $HOME/create_project_$PROJECTNAME.log
     else
     	echo "Unable to find Git OAuth token. No Github repo created"
     fi
 
     echo "Project: $PROJECTNAME created."
+    echo "For a detailed logfile, look here:  $HOME/create_project_$PROJECTNAME.log"
 fi
