a383d26499e617abdb0558415224a70450ffbcca
Morten Egan <morten@egan.dk>
2013-10-19 11:52:51 +0200

    Added scripts

---
+++ Scripts/nas_mount.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo mount 10.0.100.12:/volume1/photo /media/morten/photo
+sudo mount 10.0.100.12:/volume1/video /media/morten/video
+sudo mount 10.0.100.12:/volume2/video2 /media/morten/video2
